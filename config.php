191d6fd8ad6df6c89add4c5b2afaa1a2f50f5dab
Mateus Daniel <35970984+MateusDanieel@users.noreply.github.com>
2020-05-22 09:54:04 -0300

    Update config.php

--- config.php
+++ config.php
@@ -7,8 +7,8 @@
 
 
 //constantes para o pagSeguro
-const PS_EMAIL = "amauridesouzaferreira@gmail.com"; //email pagseguro
-const PS_TOKEN = "866415C4ED324EA7A5C0D3BAB1FE4EC3"; //token produção
+const PS_EMAIL = ""; //email pagseguro
+const PS_TOKEN = ""; //token produção
 const PS_TOKEN_SBX = ""; //token do sandbox 
 const PS_AMBIENTE = "production"; //production sandbox
 
